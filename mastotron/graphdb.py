0410531ac12c11b247bc96f03d47b77c2944bc73
Ryan Heuser <ryan.heuser@gmail.com>
2023-01-01 09:32:04 -0500

    tinydb stinx

--- mastotron/graphdb.py
+++ mastotron/graphdb.py
@@ -1,4 +1,5 @@
 from .imports import *
+from tinydb import TinyDB, Query
 
 class GraphDB:
     def __init__(self, account_name_or_mastotron_obj, name='graphdb'):
@@ -15,25 +16,26 @@ class GraphDB:
         # import networkdisk as nd
         # self._G = nd.sqlite.DiGraph(db=self.path, name='graphdb')
         from cog.torque import Graph
-        from sqlitedict import SqliteDict
         self._g = Graph(self.name, cog_home=os.path.basename(self.path_g), cog_path_prefix=os.path.dirname(self.path_g))
-        self._db = SqliteDict(self.path_db, autocommit=True)
+        
+        # from sqlitedict import SqliteDict
+        # self._db = SqliteDict(self.path_db, autocommit=True)
         # self._db.create_or_load_namespace(f"{self.name}_ns")
         # self._db.create_table(f"{self.name}_db", f"{self.name}_ns")
-
+        self._db = TinyDB(self.path_db)
 
     @property
     def path_g(self):
         return os.path.join(self._tron.path_acct, 'cog.graph')
     @property
     def path_db(self):
-        return os.path.join(self._tron.path_acct, 'db.sqlitedict')
+        return os.path.join(self._tron.path_acct, 'tinydb.json')
 
     @property
     def g(self): return self._g
     @property
     def db(self): return self._db
-    d = db
+    # d = db
 
     def update(self, timeline_type='home', max_posts=100, hours_ago=24):
         for post in self._tron.iter_timeline(
@@ -43,8 +45,16 @@ class GraphDB:
             self.ingest_post(post, timeline_type=timeline_type)
 
     def ingest_post(self, post, force=False, **extra):
-        if force or not post.uri in self.d:
-            print(f'>> ingesting: {post.uri}')
-            self.d[post.uri] = {**post.data, **extra}
-            self.d[post.author.uri] = post.author.data
-            self.g.put(post.author.uri,'posted',post.uri)
+        # if force or not post.uri in self.d:
+        print(f'>> ingesting: {post.uri}')
+        Q = Query()
+        self.db.upsert(
+            {**post.data, **extra},
+            Q.uri == post.uri
+        )
+        # self.d[post.author.uri] = post.author.data
+        self.db.upsert(
+            post.author.data,
+            Q.uri == post.author.uri
+        )
+        self.g.put(post.author.uri,'posted',post.uri)
