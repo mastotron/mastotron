dff312ef18cc1d9e641e5e7deefdf67894a9d5f4
Ryan Heuser <ryan.heuser@gmail.com>
2023-02-04 15:57:32 -0500

    reorging

--- gui/src/preload.js
+++
@@ -1,5 +0,0 @@
-// A way of safely requiring nodejs stuff in the render process
-// rather than opening up safety problems with contextIsolation: true
-// We can keep contextIsolation: false in src/index.js and still get access here.
-// See https://stackoverflow.com/questions/54544519/electron-require-is-not-defined
-window.ipcRenderer = require('electron').ipcRenderer;
